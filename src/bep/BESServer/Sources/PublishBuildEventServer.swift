65f6b4e9e0cc5d6571117232cb88c9cc3c57f031
Connor Wybranowski <cwybro@gmail.com>
2023-06-26 23:12:34 -0400

    Integrate proto targets in server and client

--- src/bep/BESServer/Sources/PublishBuildEventServer.swift
+++ src/bep/BESServer/Sources/PublishBuildEventServer.swift
@@ -7,9 +7,12 @@ public enum PublishBuildEventServer {
         // [CW] 2/20/23 - Based on:
         // https://github.com/grpc/grpc-swift/blob/main/Sources/Examples/Echo/Runtime/Echo.swift#L120
         let builder: Server.Builder = Server.insecure(group: MultiThreadedEventLoopGroup(numberOfThreads: 1))
-        let server = try await builder.withServiceProviders([PublishBuildEventProviderImp()])
-            .bind(host: "localhost", port: 9000)
-            .get()
+        let server = try await builder.withServiceProviders([
+            PublishBuildEventProviderImp(),
+            HealthProviderImp()
+        ])
+        .bind(host: "localhost", port: 9000)
+        .get()
         print("Started BES server: \(server.channel.localAddress!)")
         try await server.onClose.get()
     }
